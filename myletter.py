a9d7d204d7638adb6f1e55de7f21adf56b2ad558
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-21 11:34:11 -0400

    Update myletter.py

--- myletter.py
+++ myletter.py
@@ -4,8 +4,8 @@ spaces = ' ' * 25
 print('%s 12 Butts Wynd' % spaces)
 print('%s Twinklebottom Heath' % spaces)
 print('%s West Snoring' % spaces)
-print
-print
+print()
+print()
 print("Dear Sir")
 print()
 print("I wish to report that tiles are missing from the outside toilet roof.")
