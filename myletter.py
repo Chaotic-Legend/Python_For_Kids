d11ce98240d8776e0cf926c426c7ab097235d0a7
Isaac Hoyos <gamemasterx40@gmail.com>
2022-09-22 13:21:11 -0400

    Update myletter.py

--- myletter.py
+++ myletter.py
@@ -1,14 +1,14 @@
 # This program is a made-up funny letter! LOL!
 
-spaces = ' ' * 25 
-print('%s 12 Butts Wynd' % spaces)
-print('%s Twinklebottom Heath' % spaces)
-print('%s West Snoring' % spaces)
+spaces = " " * 25 
+print("%s 12 Butts Wynd" % spaces)
+print("%s Twinklebottom Heath" % spaces)
+print("%s West Snoring" % spaces)
 print()
 print()
 print("Dear Sir")
 print()
 print("I wish to report that tiles are missing from the outside toilet roof.")
 print()
-print("Regrads")
+print("Regrads,")
 print("Malcom Dithering")
